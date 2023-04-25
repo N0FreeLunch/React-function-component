83415569fe1e2d36599cb924e6f9441a849e0cf1
N0FreeLunch <consilience90@gmail.com>
2023-04-25 21:17:28 +0900

    지정한 컴포넌트 번호로 이동할 수 있게 변경

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import componentList from './componentList.jsx';
 
 function App() {
   const [componentNumber, setComponentNumber] = useState(componentList.length);
+  const [inputNumber, setInputNumber] = useState();
 
   const next = () => {
     if(componentNumber < componentList.length) {
@@ -16,11 +17,47 @@ function App() {
     }
   }
 
+  const move = () => {
+    if(1 <= inputNumber && inputNumber <= componentList.length) {
+      setComponentNumber(inputNumber)
+    }
+  }
+
+  const storeInputtNumber = (inputEvent) => {
+    setInputNumber(parseInt(inputEvent.target.value));
+  }
+
+  const style = {
+    numberDisplay : {
+      marginLeft: '10px'
+    },
+    prevNextBtn : {
+      marginLeft: '10px'
+    },
+    inputTitle : {
+      marginLeft: '10px'
+    },
+    input : {
+      width: '80px',
+      marginRight: '20px',
+      marginLeft: '10px'
+    }
+  };
+
   return (
     <div>
-      <h3>current compoenent number : {componentNumber}</h3>
-      <button onClick={prev}>prev</button>
-      <button onClick={next}>next</button>
+      <h3 style={style.numberDisplay}>current compoenent number : {componentNumber}</h3>
+      <div style={style.prevNextBtn}>
+        <button onClick={prev}>prev</button>
+        <button onClick={next}>next</button>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+        <div style={style.inputTitle}>input component number</div>
+        <input type='number' min='1' max={componentList.length} onChange={storeInputtNumber} style={style.input}></input>
+        <button type='button' onClick={move}>move</button>
+      </div>
       <br/><br/><br/>
       <div>
         {componentList[componentNumber-1]}
