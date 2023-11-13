a5b70ccc0f79d575bb2079593b13c091c224f670
N0FreeLunch <consilience90@gmail.com>
2023-11-13 22:16:56 +0900

    예제에서 필요 없는 코드 삭제

--- src/components/20-classComponentProps/ParentComponent.js
+++ src/components/20-classComponentProps/ParentComponent.js
@@ -3,7 +3,6 @@ import ChildComponent from './ChildComponent.js';
 
 class ParentComponent extends Component {
   render() {
-    const { name, favoriteNumber, children } = this.props;
     return (
       <div>
         <ChildComponent favoriteNumber={30}>
