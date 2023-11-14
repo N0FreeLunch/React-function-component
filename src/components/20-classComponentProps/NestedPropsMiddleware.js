94271f7da4908ab091e9c4dc769af1477b84c3fd
N0FreeLunch <consilience90@gmail.com>
2023-11-14 22:41:54 +0900

    props 설정을 static 메소드로 하는 예제 추가

---
+++ src/components/20-classComponentProps/NestedPropsMiddleware.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NestedPropsMiddleware extends Component {
+  static defaultProps = {
+    name: 'default name',
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        NestedPropsMiddleware <br />
+        Hi, my name is {name}. <br />
+        children value is {children}
+        <br />
+        my favorite number is {favoriteNumber}
+      </div>
+    );
+  }
+}
+
+export default NestedPropsMiddleware;
--- src/components/20-classComponentProps/ParentComponent.js
+++ src/components/20-classComponentProps/ParentComponent.js
@@ -1,14 +1,22 @@
 import { Component } from 'react';
 import ChildComponent from './ChildComponent';
+import NestedPropsMiddleware from './NestedPropsMiddleware';
 
 class ParentComponent extends Component {
   render() {
     return (
-      <div>
-        <ChildComponent favoriteNumber={30}>
-          class component children value
-        </ChildComponent>
-      </div>
+      <>
+        <div style={{ borderStyle: 'solid' }}>
+          <ChildComponent favoriteNumber={30}>
+            class component children value
+          </ChildComponent>
+        </div>
+        <div style={{ borderStyle: 'solid' }}>
+          <NestedPropsMiddleware favoriteNumber={30}>
+            class component children value
+          </NestedPropsMiddleware>
+        </div>
+      </>
     );
   }
 }
